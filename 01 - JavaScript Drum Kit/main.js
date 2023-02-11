a8e7e09555c072bd85ae223aa7a61fbb7c15f1dc
Chelsie Ty <chelsiety@gmail.com>
2023-02-11 17:13:44 +0800

    add .playing class from css stylesheet to add effects when key is pressed

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,11 +1,12 @@
 
 window.addEventListener('keydown',function(e){
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    
+    const key = document.querySelector(`.key[data-key="${e.code}"]`)
 
     if(!audio) return; // Stop the function from running if key pressed is not within scope of assigned audio keys
     audio.currentTime = 0;  // Rewind to the start 0f the audio clip. Set time position to 0 seconds
     audio.play();
+    key.classList.add('playing');  // add .playing class from css stylesheet to add effects when key is pressed
 })
 
 
