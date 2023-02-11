16eb77820162ceb6f002bcb808200d007e49665d
Chelsie Ty <chelsiety@gmail.com>
2023-02-11 14:49:04 +0800

    Add audio functionalities
    - Make the function stop running if the key pressed is not within the assigned audio keys
    - Set the time position to 0 seconds using .currentTime. Rewind audio.
    - Add the play() functionality that plays the audio when corresponding key is pressed.

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,10 +1,11 @@
 
 window.addEventListener('keydown',function(e){
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    console.log(e);
-    console.log(e.code);
-    console.log(audio);
     
+
+    if(!audio) return; // Stop the function from running if key pressed is not within scope of assigned audio keys
+    audio.currentTime = 0;  // Rewind to the start 0f the audio clip. Set time position to 0 seconds
+    audio.play();
 })
 
 
